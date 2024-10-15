341e831edde7b231e2c91754369bda661bb8e774
Nguyen Van Hiep <hiepnv@gostream.vn>
2024-10-15 08:16:20 +0700

    add

--- tap.js
+++ tap.js
@@ -11,8 +11,8 @@ function getRandomOffset() {
 function autoClick() {
   const randomX = baseX + getRandomOffset();
   const randomY = baseY + getRandomOffset();
-  robot.moveMouseSmooth(randomX, randomY);
-  // robot.moveMouse(randomX, randomY);
+  // robot.moveMouseSmooth(randomX, randomY);
+  robot.moveMouse(randomX, randomY);
   robot.mouseClick();
 
   const randomTime = Math.floor(Math.random() * 100) + 150
